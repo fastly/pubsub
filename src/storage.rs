34279ff686f1531feffcb8f121098d6b2797e6fc
Justin Karneges <jkarneges@fastly.com>
2025-04-24 09:40:04 -0700

    add storage module for retaining messages in kv store

---
+++ src/storage.rs
@@ -0,0 +1,248 @@
+use fastly::kv_store::{InsertMode, KVStoreError, LookupResponse};
+use fastly::KVStore;
+use std::time::Duration;
+
+const KV_STORE_MAX_AGE_SECS: u64 = 60 * 60;
+const TTL_EXTEND: Duration =
+    Duration::from_secs(KV_STORE_MAX_AGE_SECS + (KV_STORE_MAX_AGE_SECS / 2));
+const WRITE_TRIES_MAX: usize = 5;
+
+#[derive(Debug)]
+pub enum StorageError {
+    StoreNotFound,
+    TooManyRequests,
+    InvalidMetadata,
+    KVStore(KVStoreError),
+}
+
+pub struct RetainedVersion {
+    pub generation: u64,
+    pub seq: u64,
+}
+
+pub struct Retained {
+    pub version: RetainedVersion,
+    pub ttl: Option<Duration>,
+    pub message: Vec<u8>,
+}
+
+#[derive(Debug, Default, serde::Deserialize, serde::Serialize)]
+struct Metadata {
+    generation: u64,
+    seq: u64,
+
+    #[serde(rename = "expires-at", skip_serializing_if = "Option::is_none")]
+    expires_at: Option<time::UtcDateTime>,
+}
+
+fn lookup(
+    store: &KVStore,
+    key_name: &str,
+) -> Result<Option<(LookupResponse, Metadata)>, StorageError> {
+    let lookup = match store.lookup(key_name) {
+        Ok(l) => l,
+        Err(KVStoreError::ItemNotFound) => return Ok(None),
+        Err(e) => return Err(StorageError::KVStore(e)),
+    };
+
+    let meta = match lookup.metadata() {
+        Some(data) => match serde_json::from_slice(&data) {
+            Ok(v) => v,
+            Err(_) => return Err(StorageError::InvalidMetadata),
+        },
+        None => return Err(StorageError::InvalidMetadata),
+    };
+
+    Ok(Some((lookup, meta)))
+}
+
+pub trait Storage {
+    fn write_retained(
+        &self,
+        topic: &str,
+        message: &[u8],
+        ttl: Option<Duration>,
+    ) -> Result<RetainedVersion, StorageError>;
+
+    fn read_retained(&self, topic: &str) -> Result<Option<Retained>, StorageError>;
+}
+
+pub struct KVStoreStorage {
+    store_name: String,
+}
+
+impl KVStoreStorage {
+    pub fn new(store_name: &str) -> Self {
+        Self {
+            store_name: store_name.to_string(),
+        }
+    }
+}
+
+impl Storage for KVStoreStorage {
+    fn write_retained(
+        &self,
+        topic: &str,
+        message: &[u8],
+        ttl: Option<Duration>,
+    ) -> Result<RetainedVersion, StorageError> {
+        let store = match KVStore::open(&self.store_name) {
+            Ok(Some(store)) => store,
+            Ok(None) => return Err(StorageError::StoreNotFound),
+            Err(e) => return Err(StorageError::KVStore(e)),
+        };
+
+        let key_name = format!("r:{}", topic);
+
+        let expires_at = ttl.map(|ttl| time::UtcDateTime::now() + ttl);
+
+        let mut tries = 0;
+
+        let version = loop {
+            let (mut meta, generation) = match lookup(&store, &key_name)? {
+                Some((lookup, meta)) => (meta, Some(lookup.current_generation())),
+                None => (Metadata::default(), None),
+            };
+
+            let insert = store.build_insert();
+
+            let insert = if let Some(generation) = generation {
+                meta.seq += 1;
+
+                insert.if_generation_match(generation)
+            } else {
+                meta.generation = rand::random();
+                meta.seq = 1;
+
+                insert.mode(InsertMode::Add)
+            };
+
+            meta.expires_at = expires_at;
+
+            let meta_json =
+                serde_json::to_string(&meta).expect("metadata should always be serializable");
+
+            let insert = insert.metadata(&meta_json);
+
+            let insert = if let Some(ttl) = ttl {
+                insert.time_to_live(ttl + TTL_EXTEND)
+            } else {
+                insert
+            };
+
+            match insert.execute(&key_name, message.to_vec()) {
+                Ok(()) => {
+                    break RetainedVersion {
+                        generation: meta.generation,
+                        seq: meta.seq,
+                    }
+                }
+                Err(KVStoreError::ItemPreconditionFailed) => {}
+                Err(KVStoreError::TooManyRequests) => {}
+                Err(e) => return Err(StorageError::KVStore(e)),
+            }
+
+            tries += 1;
+
+            if tries >= WRITE_TRIES_MAX {
+                // getting conflicts or rate limit errors after several tries
+                return Err(StorageError::TooManyRequests);
+            }
+        };
+
+        Ok(version)
+    }
+
+    fn read_retained(&self, topic: &str) -> Result<Option<Retained>, StorageError> {
+        let store = match KVStore::open(&self.store_name) {
+            Ok(Some(store)) => store,
+            Ok(None) => return Err(StorageError::StoreNotFound),
+            Err(e) => return Err(StorageError::KVStore(e)),
+        };
+
+        let key_name = format!("r:{}", topic);
+
+        let (mut lookup, meta) = match lookup(&store, &key_name)? {
+            Some(ret) => ret,
+            None => return Ok(None),
+        };
+
+        let mut ttl = None;
+
+        if let Some(expires_at) = meta.expires_at {
+            let now = time::UtcDateTime::now();
+
+            if now >= expires_at {
+                // ignore the value until it gets cleaned up
+                return Ok(None);
+            }
+
+            ttl = Some((expires_at - now).unsigned_abs());
+        }
+
+        let value = lookup.take_body_bytes();
+
+        Ok(Some(Retained {
+            version: RetainedVersion {
+                generation: meta.generation,
+                seq: meta.seq,
+            },
+            ttl,
+            message: value,
+        }))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str;
+
+    #[test]
+    fn retained() {
+        let storage = KVStoreStorage::new("messages");
+
+        assert!(storage.read_retained("storage-test").unwrap().is_none());
+
+        let v1 = storage
+            .write_retained("storage-test", "hello".as_bytes(), None)
+            .unwrap();
+        assert_eq!(v1.seq, 1);
+
+        let r = storage.read_retained("storage-test").unwrap().unwrap();
+        assert_eq!(r.version.generation, v1.generation);
+        assert_eq!(r.version.seq, 1);
+        assert!(r.ttl.is_none());
+        assert_eq!(str::from_utf8(&r.message).unwrap(), "hello");
+
+        let v2 = storage
+            .write_retained(
+                "storage-test",
+                "world".as_bytes(),
+                Some(Duration::from_secs(60)),
+            )
+            .unwrap();
+        assert_eq!(v2.generation, v1.generation);
+        assert_eq!(v2.seq, 2);
+
+        let r = storage.read_retained("storage-test").unwrap().unwrap();
+        assert_eq!(r.version.generation, v2.generation);
+        assert_eq!(r.version.seq, 2);
+        let ttl = r.ttl.unwrap();
+        assert!(ttl <= Duration::from_secs(60));
+        assert_eq!(str::from_utf8(&r.message).unwrap(), "world");
+
+        // delete item so next write gets a new generation
+        KVStore::open(&storage.store_name)
+            .unwrap()
+            .unwrap()
+            .delete("r:storage-test")
+            .unwrap();
+
+        let new_v1 = storage
+            .write_retained("storage-test", "hello".as_bytes(), None)
+            .unwrap();
+        assert!(new_v1.generation != v1.generation);
+        assert_eq!(new_v1.seq, 1);
+    }
+}
